a00de67e7729fbf6ec16b5a5d02d636fa82fd768
Hwirin-Kim <kimhuirin@gmail.com>
2023-10-05 21:00:50 +0900

    feat : SEO 설정 완료 fix : search page 다이나믹설정 완료

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,5 +1,8 @@
 import { searchUsers } from "@/service/user";
 import { NextResponse } from "next/server";
+
+//해당 api는 요청이 올때마다 캐시된걸 사용하지 않고 새로 요청을 보내야 하므로 dynamic하게 설정
+export const dynamic = "force-dynamic";
 export async function GET() {
   //로그인 하지 않아도 사용할수있도록 session정보 안가져옴
   return searchUsers().then((data) => NextResponse.json(data));
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,14 @@
 import UserSearch from "@/components/UserSearch";
+import { Metadata } from "next";
 import React from "react";
 
+//해당 페이지는 항상 dynamic하게 작동해야 하므로 추가
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Search",
+  description: "사용자 검색",
+};
 export default function SearchPage() {
   return <UserSearch />;
 }
